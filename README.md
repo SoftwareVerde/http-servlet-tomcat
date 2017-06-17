c97cc49800a919138a2570f38a5f5a84097921a9
Josh Green <josh@softwareverde.com>
2017-06-17 12:10:23 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Http-Servlet-Tomcat
+
+Http-Servlet-Tomcat is a migration shim for Java-Servlet to Apache Tomcat.
+
---
+++ src/main/java/com/softwareverde/httpserver/tomcat/request/TomcatRequest.java
@@ -0,0 +1,53 @@
+package com.softwareverde.httpserver.tomcat.request;
+
+import com.softwareverde.http.cookie.Cookie;
+import com.softwareverde.servlet.GetParameters;
+import com.softwareverde.servlet.PostParameters;
+import com.softwareverde.servlet.request.Request;
+import com.softwareverde.util.Util;
+
+import java.util.List;
+import java.util.Map;
+
+public class TomcatRequest extends Request {
+    public void setHostname(final String hostname) {
+        _hostname = hostname;
+    }
+
+    public void setFilePath(final String filePath) {
+        _filePath = filePath;
+    }
+
+    public void setMethod(final HttpMethod method) {
+        _method = method;
+    }
+
+    public void setHeaders(final Map<String, List<String>> headers) {
+        _headers.clear();
+        for (final String key : headers.keySet()) {
+            final List<String> values = headers.get(key);
+            _headers.put(key, Util.copyList(values));
+        }
+    }
+
+    public void setCookies(final List<Cookie> cookies) {
+        _cookies.clear();
+        _cookies.addAll(cookies);
+    }
+
+    public void setGetParameters(final GetParameters getParameters) {
+        _getParameters = getParameters;
+    }
+
+    public void setRawQueryString(final String queryString) {
+        _rawQueryString = queryString;
+    }
+
+    public void setPostParameters(final PostParameters postParameters) {
+        _postParameters = postParameters;
+    }
+
+    public void setRawPostData(final byte[] rawPostData) {
+        _rawPostData = rawPostData;
+    }
+}
